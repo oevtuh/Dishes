625ab34cc915c99c505638e06bebe629a1f91679
olegzxz <oevtuh@gmail.com>
2014-12-26 00:44:09 +0200

    ajax method works with category-box

--- Dishes/Scripts/choose.js
+++ Dishes/Scripts/choose.js
@@ -1,8 +1,21 @@
 ﻿(function ($) {
    
-    $(".chzn-select").chosen();
-    $(".chzn-select").change(selectDishes);
+    //$(".chzn-select").chosen();
+    //$(".chzn-select").change(selectDishes);
+  
+    var selected = [];
+    $(".category-box").change(function () {
+      
+        selected = [];
+        $('.category-box input:checked').each(function () {
+            selected.push(+$(this).attr('value'));
+        });
 
+        selectDishes();
+    });
+
+   
+    
     var viewModel = new ViewModel();
 
     ko.applyBindings(viewModel);
@@ -14,7 +27,8 @@
             url: 'FindByIngredients',
             type: 'get',
             data: {
-                dishes: $(".chzn-select").val(),
+                //dishes: $(".chzn-select").val(),
+                dishes: selected,
             },
             traditional: true,
             dataType: 'json',
